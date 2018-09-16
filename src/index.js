faf16f541a0470f23e3c673a97ecaf2124f90d79
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-17 00:34:07 +0300

    forth is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var arr = equation.split(" ");
+    var result = [];
+    var a = +arr[0];
+    var firstSign = arr[3];
+    var b = +arr[4]
+    var secondSign = arr[7];
+    var c = +arr[8];
+    if(firstSign == "-"){
+      b = -b;
+    }
+    if(secondSign == "-"){
+      c = -c;
+    }
+    var D = Math.pow(b,2) - 4 * a * c;
+    if(D > 0){
+      x1 = Math.round((-b - Math.sqrt(D)) / (2*a));
+      x2 = Math.round((-b + Math.sqrt(D)) / (2*a));
+      if (x1>x2){
+        result.push(x2);
+        result.push(x1);
+      }
+      else{
+        result.push(x1);
+        result.push(x2);
+      }
+    }
+    else if(D=0){
+      x = (-b + Math.sqrt(D)) / (2*a);
+      result.push(x);
+    }
+    else{
+
+    }
+    return result;
 }
